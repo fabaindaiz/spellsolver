04f696663a693869a33753781a299c5591e6c586
Yılmaz ÇABUK <yilmazcabuk@protonmail.com>
2023-08-09 13:14:08 +0300

    refactor: add unit tests and improve code readability
    Added unit tests for utility functions in newly created `utils_test.py` and `iteration_test.py`. This was done to improve overall code reliability and ensure that the functions were behaving as expected.
    
    Also, refactored the `utils.py` and `iteration.py` code for better readability. This involved:
    - Updating the `POINTS` dictionary structure for improved visibility.
    - Extended the docstring for the `pairwise` function for comprehensive understanding of its use-case and behavior.
    
    Furthermore, renamed `itertools.py` to `iteration.py` to avoid confusion with Python's built-in `itertools` module.

--- src/utils/itertools.py
+++
@@ -1,9 +0,0 @@
-from itertools import tee
-from typing import Iterable
-
-
-def pairwise(iterable: Iterable) -> Iterable:
-    """pairwise('ABCDEFG') --> AB BC CD DE EF FG"""
-    a, b = tee(iterable)
-    next(b, None)
-    return zip(a, b)
