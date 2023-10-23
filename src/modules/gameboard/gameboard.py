e211781de1537b5ba1c6827f797ba46e882adf43
Yılmaz ÇABUK <yilmazcabuk@protonmail.com>
2023-10-23 18:16:24 +0300

    refactor: remove duplicated code
    - Moved the GameTile class and all its associated code to a new separate gametile.py file from gameboard.py.
    - Removed unnecessary NEIGHBOR_OFFSETS constant from gameboard.py.
    - Simplified the gameboard.py module by removing redundant import declarations and reformatting the syntax.

--- src/modules/gameboard/gameboard.py
+++ src/modules/gameboard/gameboard.py
@@ -1,56 +1,10 @@
-from typing import Generator, Dict, List, Tuple
+from typing import Dict, Tuple
 
+from src.modules.gameboard.gametile import GameTile
 from src.utils.utils import (
     aux_to_indices,
-    get_letter_point_value,
     is_valid_word,
 )
-
-NEIGHBOR_OFFSETS = [
-    (x, y) for x in range(-1, 2) for y in range(-1, 2) if (x, y) != (0, 0)
-]
-
-
-class GameTile:
-    """Respresents a Spellcast tile"""
-
-    def __init__(self, letter: str, cord: Tuple[int, int]) -> None:
-        self.letter: str = letter
-        self.cord: Tuple[int, int] = cord
-        self.swap: bool = False
-
-        self.letter_points: int = get_letter_point_value(letter)
-        self.letter_mult: int = 1
-        self.word_mult: int = 1
-
-        self.neighbors: List[GameTile] = []
-
-    def copy(self, letter: str) -> "GameTile":
-        """Makes a copy of a Gametile"""
-        node = GameTile(letter, self.cord)
-        node.swap = True
-
-        node.letter_mult = self.letter_mult
-        node.word_mult = self.word_mult
-        return node
-
-    def points(self) -> int:
-        """Gets points value of actual tile"""
-        return self.letter_points * self.letter_mult
-
-    def init_neighbors(self, tiles: Dict[Tuple[int, int], "GameTile"]) -> None:
-        """Init neighbors of actual tile"""
-        x, y = self.cord
-        neighbors_cords = (
-            (x + dx, y + dy)
-            for dx, dy in NEIGHBOR_OFFSETS
-            if 0 <= x + dx < 5 and 0 <= y + dy < 5
-        )
-        self.neighbors.extend(tiles[cord] for cord in neighbors_cords)
-
-    def suggest_tile(self, path: List["GameTile"]) -> Generator["GameTile", None, None]:
-        """Get all nodes in neighbors that are not in path"""
-        return (tile for tile in self.neighbors if tile not in path)
 
 
 class GameBoard:
